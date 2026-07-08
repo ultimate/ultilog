8abd2fd0b8d78a1511e7c4888164bbebae2bf6be
Julian V <ultimatej@gmx.net>
2026-07-08 23:12:57 +0200

    Remove unused boat field order template

--- app/templates/boat-data.ts
+++
@@ -1,1 +0,0 @@
-export const yachtDataOrder = ["Class / type", "MMSI", "Manufacturer", "Hull length", "Beam", "Draft", "Displacement", "Rig / sail area", "Engine", "Propeller", "Electronics", "Safety"];
