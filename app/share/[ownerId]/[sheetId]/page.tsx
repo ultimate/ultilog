418896d52695b92711bb1b9c48928bb3f1402db4
Julian V <ultimatej@gmx.net>
2026-07-16 16:58:38 +0200

    Fix shared route slug conflict

--- app/share/[ownerId]/[sheetId]/page.tsx
+++
@@ -1,6 +0,0 @@
-import SharedLogbookPage from "../../[sheetId]/page";
-
-export default async function OwnerScopedSharedLogbookPage({ params }: { params: Promise<{ ownerId: string; sheetId: string }> }) {
-  const { ownerId, sheetId } = await params;
-  return <SharedLogbookPage params={Promise.resolve({ ownerId, sheetId })} />;
-}
